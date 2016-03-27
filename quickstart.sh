db0d39dbead4f049af80665c207e55360de61ec0
yayoiukai <yayoi.ukai@gmail.com>
2016-03-27 13:38:01 -0700

    add wait for postgres

--- quickstart.sh
+++ quickstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker-compose build
 docker-compose up -d
+sleep 2
 docker-compose run web python manage.py migrate
 echo ">> Application is running <<"
 echo $DOCKER_HOST | sed 's/tcp:\/\//http:\/\//g'
