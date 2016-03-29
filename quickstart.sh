c58a7ffb7531afb0e8758e90fcaf0da7db0435d9
yayoiukai <yayoi.ukai@gmail.com>
2016-03-28 22:44:25 -0700

    updated quickstart.sh file

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 docker-compose build
-docker-compose up -d
+docker-compose up -d db
 sleep 2
-docker-compose run web python manage.py migrate
+docker-compose run web python manage.py migrate || exit 1
+docker-compose up -d
 echo ">> Application is running <<"
-echo $DOCKER_HOST | sed 's/tcp:\/\//http:\/\//g'
+echo "http://${DOCKER_HOST:-localhost}:8000" | sed 's/tcp:\/\///g' | sed 's/:[0-9]+//'
